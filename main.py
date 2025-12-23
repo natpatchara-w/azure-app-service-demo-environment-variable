72ace05b3454178c77440f0e5c5f433bfd74b681
natpatchara.pon <n.pongjirapat@gmail.com>
2025-12-23 13:50:03 +0700

    Updated api: add environ api for query environment variable
    - Add: /env/{env_var}
      Add env api for query environment variable
    - Minor fixed:
      Add uvicorn query for easier startup(for tutorial and local testing)

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import os
+
 import fastapi
+import uvicorn
 
 app = fastapi.FastAPI()
 
 @app.get("/")
 def home():
     return {"Hello"}
+
+@app.get("/env/{env_var}")
+def get_env(env_var: str):
+    return {
+            "var_name": env_var,
+            "value": os.environ.get(env_var, "not found")
+            }
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
